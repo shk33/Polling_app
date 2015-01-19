df6cb71388d9e9580b4ccd420239631c1b84aff4
shk33 <m.coronel.seg@gmail.com>
2015-01-18 18:42:39 -0600

    Add possible answers model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ActiveRecord::Base
+  has_many :possible_answers
   belongs_to :poll
+  accepts_nested_attributes_for :possible_answers, reject_if: proc { |attributes| attributes['title'].blank? }
 end
