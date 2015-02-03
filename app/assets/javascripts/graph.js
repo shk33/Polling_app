b7c77a7ea0a2c7de56a0c0935188d5e9ed06712a
shk33 <m.coronel.seg@gmail.com>
2015-02-02 22:40:44 -0600

    Refactor graph.js and add devise gem

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -17,6 +17,14 @@ Graph.prototype.getData = function() {
   return data;
 };
 
+Graph.prototype.getKind = function() {
+  var kinds = {
+    "column": "ColumnChart",
+    "pie":    "PieChart"
+  }
+  return kinds[this.kind]
+};
+
 Graph.prototype.render  = function() {
 
   // Set chart options
@@ -25,7 +33,7 @@ Graph.prototype.render  = function() {
                  'height':300};
 
   // Instantiate and draw our chart, passing in some options.
-  var chart = new google.visualization.ColumnChart($(this.selector)[0]);
+  var chart = new google.visualization[this.getKind()]($(this.selector)[0]);
   chart.draw(this.getData(), options);
 };
 
